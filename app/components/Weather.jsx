ada24549b585748eff05c80aabedc129b5b05dfb
marcint87 <kulio2@wp.pl>
2016-12-19 16:06:44 +0100

    added modal pop up for error message

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var ErrorModal = require('ErrorModal');
 var openWeatherMap = require('openWeatherMap');
 
 
@@ -16,7 +17,10 @@ var Weather = React.createClass({
 	handleSearch: function (location) {
 		var that = this;
 
-		this.setState({isLoading: true});
+		this.setState({
+			isLoading: true,
+			errorMessage: undefined
+		});
 
 
 		openWeatherMap.getTemp(location).then(function(temp) {
@@ -27,15 +31,15 @@ var Weather = React.createClass({
 			});
 
 		}, function (errorMessage) {
-			alert(errorMessage);
 			that.setState({
-				isLoading: false
+				isLoading: false,
+				errorMessage: errorMessage.message
 			});
 		});
 	},
 
 	render: function () {
-		var {isLoading, temp, location} = this.state;
+		var {isLoading, temp, location, errorMessage} = this.state;
 
 		function renderMessage() {
 
@@ -47,11 +51,19 @@ var Weather = React.createClass({
 				return <WeatherMessage temp={temp} location={location} />;
 			}
 		}
+		function renderError () {
+			if(typeof errorMessage === 'string' ) {
+				return (
+					<ErrorModal/>
+					);
+			}
+		}
 			return (
 				<div>
 					<h1 className="text-center">Weather Component</h1>
 					<WeatherForm onSearch={this.handleSearch}/>
 					{renderMessage()}
+					{renderError()}
 				</div>
 			);
 	}	
