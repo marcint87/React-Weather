68a33ac10884ae798e170139f2fe3649aa9dd958
marcint87 <kulio2@wp.pl>
2016-12-19 15:00:25 +0100

    centered components

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require("react-router");
 
 var Examples = (props) => {
 	return (
 			<div>
-				<h3>Examples</h3>
-				<p>new paragraph</p>
+				<h1 className="text-center">Examples</h1>
+				<p className="text-center">few example locations to try out</p>
+				<ol>
+					<li>
+						<Link to="/?location=Białystok">Białystok</Link>
+					</li>
+					<li>
+						<Link to="/?location=Rio">Rio de Janeiro</Link>
+					</li>
+				</ol>
 			</div>
 		);
 }
